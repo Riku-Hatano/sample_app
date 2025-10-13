b5a3c17d61df85fa6ac9aa1414e020ec85046cf9
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2025-10-13 19:37:39 +0900

    小文字にするコールバックを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,7 +29,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email validation should accept valid addresses" do
+  test 'email validation should accept valid addresses' do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -38,12 +38,25 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email validation should reject invalid addresses" do
+  test 'email validation should reject invalid addresses' do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test 'mail address should be unique' do
+    dupulicate_user = @user.dup
+    @user.save
+    assert_not dupulicate_user.valid?
+  end
+
+  test 'email addresses should be saved as lowercase' do
+    upper_case_email = 'TEST@EXAMPlE.COM'
+    @user.email = uppper_case_email
+    @user.save
+    assert_equal upper_case_email.downcase, @user.reload.email
   end
 end
